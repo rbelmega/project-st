43a8de8e531a2166501c87f1edd9a99139391a2e
unknown <snykyf@IF075.softservecom.com>
2015-01-14 11:58:13 +0200

    added router, menu and made some changes in gulp tasks

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 /**
  * Created by snykyf on 1/13/2015.
  */
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/dashboard', {
+				template: '<h1>Dashboard</h1>'
+			}).
+			when('/matches', {
+				template: '<h1>Matches</h1>'
+			}).
+			when('/teams', {
+				template: '<h1>Teams</h1>'
+			}).
+			when('/championship', {
+				template: '<h1>Championship</h1>'
+			}).
+			otherwise({
+				redirectTo: '#'
+			});
+	}]);
