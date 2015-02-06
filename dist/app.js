d69ee19824a061dd9b2e047478699064546a062e
StanislavNykyforuk <stasnykyforuk@gmail.com>
2015-02-06 01:24:58 -0800

    added filtering

--- dist/app.js
+++ dist/app.js
@@ -31,18 +31,18 @@ myApp.config(['$routeProvider',
 	}])
 	.controller("SpainTeamsCtrl", ["$scope", function($scope) {
 		$scope.teams = [
-			{name: "Real Madrid", foundingDate: "1288323623006"},
-			{name: "Barcelona", foundingDate: "1288323623006"},
-			{name: "Atletico", foundingDate: "1288323623006"},
-			{name: "Valencia", foundingDate: "1288323623006"}
+			{name: "Real", city: "Madrid", foundingDate: "1288323623006"},
+			{name: "Barcelona", city: "Barcelona", foundingDate: "1288323623006"},
+			{name: "Atletico", city: "Madrid", foundingDate: "1288323623006"},
+			{name: "Valencia", city: "Valencia", foundingDate: "1288323623006"}
 		];
 	}])
 	.controller("EnglishTeamsCtrl", ["$scope", function($scope) {
 		$scope.teams = [
-			{name: "Manchester United", foundingDate: "1288323623006"},
-			{name: "Manchester City", foundingDate: "1288323623006"},
-			{name: "Arsenal", foundingDate: "1288323623006"},
-			{name: "Liverpool", foundingDate: "1288323623006"}
+			{name: "Manchester United", city: "Manchester", foundingDate: "1288323623006"},
+			{name: "Manchester City", city: "Manchester", foundingDate: "1288323623006"},
+			{name: "Arsenal", city: "London", foundingDate: "1288323623006"},
+			{name: "Liverpool", city: "Liverpool", foundingDate: "1288323623006"}
 		];
 	}])
 	.directive("teamsSquare", function() {
