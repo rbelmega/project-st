b94cbd1e757e25714e298b85e01eda27cbf9d19c
StanislavNykyforuk <stasnykyforuk@gmail.com>
2015-02-28 21:32:20 -0800

    made changes in click handler

--- src/views-controllers/championship-page.controller.js
+++ src/views-controllers/championship-page.controller.js
@@ -3,6 +3,10 @@ angular.module("views")
         function($scope, UrlsProvider, FootballInformationService) {
             $scope.championshipTitle = "Championship";
 
+            $scope.clickHandler = function() {
+                $window.alert("click");
+            }
+
             FootballInformationService.getItems(UrlsProvider.championships)
                 .then(function(championships) {
                     $scope.championships = FootballInformationService.precessChampionshipsData(championships);
