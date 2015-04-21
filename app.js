8c748b5fb0ab7d2f14d6af340b32496cf5f3fd0c
Belmeha, Rostyslav <RBELME06@iespc.ibm.com>
2015-04-21 21:26:03 +0300

    clean

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by snykyf on 1/13/2015.
+ */
+var myApp = angular.module('app', ["ui.router", "ngAnimate", "widgets", "views"]);
+
+myApp.config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+		$urlRouterProvider.otherwise('404');
+		$urlRouterProvider.when('', '/dashboard');
+		$stateProvider.
+			state('dashboard', {
+				url: '/dashboard',
+				templateUrl: 'views/dashboard.html',
+				controller: 'dashboardPageCtrl'
+			}).
+			state('matches', {
+				url: '/matches',
+				templateUrl: 'views/matches.html',
+				controller: 'matchesPageCtrl'
+			}).
+			state('teams', {
+				url: '/teams',
+				templateUrl: 'views/teams.html',
+				controller: 'teamsPageCtrl'
+			}).
+			state('championship', {
+				url: '/championship',
+				templateUrl: 'views/championship.html',
+				controller: 'championshipPageCtrl'
+			}).
+			state('404', {
+				url: '/404',
+				templateUrl: 'views/404.html'
+			});
+	}]);
